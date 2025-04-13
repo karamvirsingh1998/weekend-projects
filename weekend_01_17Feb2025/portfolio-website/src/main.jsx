180c4d746e656b34917d6c251d2d40f3ca794adc
karamvir singh <karamvirh71@gmail.com>
2025-04-13 17:18:29 +0530

    analytic

--- weekend_01_17Feb2025/portfolio-website/src/main.jsx
+++ weekend_01_17Feb2025/portfolio-website/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 )
